85801202e0740817aa0cf18246742a91acdf2dc7
Timmy Chang <timmy5519@gmail.com>
2016-08-31 17:30:50 +0800

    change command rpefix word   - using dollar sign   - closed #3 issue

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const bot = require('./bot')
 
 bot.on('start', function() {
   bot.on('message', function(data) {
-    if (/^\/\/duty/.test(data.text)) {
+    if (/^\$duty/.test(data.text)) {
       bot.getChannelById(data.channel).then((data) => {
         require('./commands/duty')(data.name)
       })
